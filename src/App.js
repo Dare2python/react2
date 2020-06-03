696b7406273c88500c90eb11096b8e9069d29bbf
dare2python <31766642+Dare2python@users.noreply.github.com>
2020-06-03 15:50:43 +0300

    implement 1st item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputState, setInputState ] = useState({
+    count: 0
+  });
+
+  // const setCount = (event) => {
+  //   setInputState({
+  //     count: event.target.value.length  
+  //   });
+  // };
+
   return (
     <div className="App">
-        <p>
-          init
-        </p>
+        <input type="text" onChange={(event) => {
+            setInputState({
+              count: event.target.value.length  
+            });
+          }}
+        />
+        <p>Count: {inputState.count}</p>
     </div>
   );
 }
